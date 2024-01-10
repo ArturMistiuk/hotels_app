f3bf8ecc3ae5868accc0e6d420f5f073be89faac
Artur Mistiuk <mistiuk.artur@gmail.com>
2024-01-10 16:16:55 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -16,6 +16,7 @@ from app.bookings.router import router as bookings_router
 from app.users.router import router as users_router
 from app.hotels.router import router as hotels_router
 from app.hotels.rooms.router import router as rooms_router
+from app.importer.router import router as importer_router
 from app.config import settings
 from app.loggger import logger
 
@@ -46,6 +47,7 @@ app.include_router(users_router)
 app.include_router(bookings_router)
 app.include_router(hotels_router)
 app.include_router(rooms_router)
+app.include_router(importer_router)
 
 app.include_router(pages_router)
 app.include_router(images_router)
